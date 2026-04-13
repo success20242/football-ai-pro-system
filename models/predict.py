74b058411895d14235895f3bb4bf0759e5c271b0
Onyekachi Chukwunyere Ebosi <ebosisuccess@gmail.com>
2026-04-14 00:23:41 +0400

    Update predict.py

--- models/predict.py
+++ models/predict.py
@@ -6,12 +6,9 @@ model = joblib.load("models/model.pkl")
 def predict(features):
 
     features = np.array(features).reshape(1, -1)
-
     probs = model.predict_proba(features)[0]
 
-    # safer generic mapping
     return {
-        "class_0": float(probs[0]),
-        "class_1": float(probs[1]),
-        "class_2": float(probs[2]),
+        "loss": float(probs[0]),
+        "win": float(probs[1])
     }
