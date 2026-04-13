0ebe53734b099fa8907bc215e21ef7b190d9570a
Onyekachi Chukwunyere Ebosi <ebosisuccess@gmail.com>
2026-04-14 02:20:11 +0400

    Update and rename elo.py to elo.py

---
+++ engine/elo.py
@@ -0,0 +1,6 @@
+def expected(r1, r2):
+    return 1 / (1 + 10 ** ((r2 - r1) / 400))
+
+
+def update(rating, expected_score, actual, k=25):
+    return rating + k * (actual - expected_score)
--- features/elo.py
+++
@@ -1,5 +0,0 @@
-def expected(r1, r2):
-    return 1 / (1 + 10 ** ((r2 - r1) / 400))
-
-def update(rating, expected, actual, k=25):
-    return rating + k * (actual - expected)
